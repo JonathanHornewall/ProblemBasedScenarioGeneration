012b47ee53a4529342fa2156ff3e9576c647e106
sdelannoypavy <solene.delannoypavy_externe@rte-france.com>
2025-10-24 13:29:15 +0200

    Update NN.jl

--- scripts/resource_allocation_prototype/method_compar/NN.jl
+++ scripts/resource_allocation_prototype/method_compar/NN.jl
@@ -18,7 +18,7 @@ function NNmodel(Nsamples,x,ξ,μᵢⱼ, cz, qw, ρᵢ)
     # Train the neural network model
     reg_param_ref = 0.0
     batchsize = 1
-    epochs = 2
+    epochs = 20
     step_size = 1e-3
     save_model_training = true
 
